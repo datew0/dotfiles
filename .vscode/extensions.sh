943a73b2302a632008aeda6d48b1405834e3d2ff
datew0 <44378542+datew0@users.noreply.github.com>
2022-07-24 02:37:32 +0300

    My config added

---
+++ .vscode/extensions.sh
@@ -0,0 +1,6 @@
+code --install-extension be5invis.vscode-icontheme-nomo-dark
+code --install-extension whizkydee.material-palenight-theme
+
+code --install-extension esbenp.prettier-vscode
+code --install-extension rafamel.subtle-brackets
+code --install-extension SirTori.indenticator
